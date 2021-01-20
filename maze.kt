495758286fd29246b2e2e4b65e3ffadd63dc3108
Bhavya Patel <71672012+Bpatel12@users.noreply.github.com>
2021-01-20 17:13:25 -0500

    Add files via upload

---
+++ maze.kt
@@ -0,0 +1,16 @@
+package gameoflife
+
+enum class CellState {
+  DEAD,
+  ALIVE
+}
+
+interface EvolutionCell {
+  val neighbours: Int
+  val state: CellState
+}
+
+interface MazeWorld {
+  fun nextGeneration(evolutionRule: EvolutionCell.() -> CellState): MazeWorld
+}
+
